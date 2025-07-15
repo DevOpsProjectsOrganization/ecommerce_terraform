97ba51be0ec868c973a603eab06b6b695f42b1af
Sridevi <parimisridevi586@gmail.com>
2025-07-15 19:25:07 +1000

    added  name

--- tools/main.tf
+++ tools/main.tf
@@ -1,8 +1,9 @@
 module "tools" {
+    for_each  = var.tools
     source = "../modules/ec2"
     ami     =  var.ami
     env     = var.env
-    instance_type= var.instance_type
+    instance_type= each.instance_type
     zone_id = var.zone_id  
     zone_name= var.zone_name 
     name    = var.name
