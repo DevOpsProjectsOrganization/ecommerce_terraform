8e5bbd292306a0352491c1ef5311b8cd7d941228
Sridevi <parimisridevi586@gmail.com>
2025-07-28 20:49:34 +1000

    added the token for vault

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -25,10 +25,10 @@ resource "aws_route53_record" "my_public_record"{
 }
 
 resource "null_resource" "ansible"{
-    triggers= {
-        always = var.env==null ? timestamp() :"false"
-    }
-    #count = var.env == null ?0 :1
+    #triggers= {
+    #   always = var.env==null ? timestamp() :"false"
+    #}
+    count = var.env == null ?0 :1
     depends_on = [aws_route53_record.my_private_record]
     provisioner "remote-exec"{
          connection {
@@ -39,7 +39,7 @@ resource "null_resource" "ansible"{
         }
         inline = [
             "sudo pip3.11 install ansible hvac",
-            "ansible-pull -i localhost, -U https://github.com/DevOpsProjectsOrganization/ecommerce_ansible roboshop.yml -e role=${var.name} "
+            "ansible-pull -i localhost, -U https://github.com/DevOpsProjectsOrganization/ecommerce_ansible roboshop.yml -e env=${var.env} -e token=${var.token} -e role=${var.name} "
         ]
        
     }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -4,4 +4,4 @@ variable "instance_type" {}
 variable "zone_id" {}
 variable "zone_name" {}
 variable "name" {}
-
+variable "token" {}
