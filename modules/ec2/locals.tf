e47e05802f3b64d18d9635ef100ce2ae13f69b86
Sridevi <parimisridevi586@gmail.com>
2025-07-30 21:15:29 +1000

    fixing errors

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -1,5 +1,5 @@
 locals {
 name = var.env==null ? var.name :"${var.name}-${var.env}"
-toolName= var.env==null ?  "${var.name}-internal" : var.name 
+toolName= var.env==null ?  "${var.name}-internal" : "${var.name}-${var.env}" 
 toolPublicName= var.env==null ?   "${var.name}-tool" : var.name
 }
